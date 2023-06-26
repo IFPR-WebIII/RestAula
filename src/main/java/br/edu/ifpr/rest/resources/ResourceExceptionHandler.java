d5db727dae0ee779f99db4ad3267a5b4f1b129a8
jefferson <jeffersong.s@hotmail.com>
2023-06-26 20:53:46 -0300

    Update project

--- src/main/java/br/edu/ifpr/rest/resources/ResourceExceptionHandler.java
+++ src/main/java/br/edu/ifpr/rest/resources/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package br.edu.ifpr.rest.resources;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
